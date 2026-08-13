330c3ca513c3091454218373515623d7742a0593
loki0985 <gudipallylokeshreddy@gmail.com>
2026-08-13 20:25:09 +0530

    Time: 333 ms (31.55%) | Memory: 113.6 MB (28.19%) - LeetSync

---
+++ 2319-longest-substring-of-one-repeating-character/longest-substring-of-one-repeating-character.cpp
@@ -0,0 +1,110 @@
+class Solution {
+    struct Node {
+        int len;
+        int pref;
+        int suff;
+        int best;
+        char leftChar;
+        char rightChar;
+
+        Node() {
+            len = pref = suff = best = 0;
+            leftChar = rightChar = 0;
+        }
+    };
+
+    vector<Node> tree;
+    string s;
+
+    Node merge(Node a, Node b) {
+        if (a.len == 0) return b;
+        if (b.len == 0) return a;
+
+        Node res;
+        res.len = a.len + b.len;
+
+        res.leftChar = a.leftChar;
+        res.rightChar = b.rightChar;
+
+        res.pref = a.pref;
+        if (a.pref == a.len && a.rightChar == b.leftChar)
+            res.pref = a.len + b.pref;
+
+        res.suff = b.suff;
+        if (b.suff == b.len && a.rightChar == b.leftChar)
+            res.suff = b.len + a.suff;
+
+        res.best = max(a.best, b.best);
+
+        if (a.rightChar == b.leftChar)
+            res.best = max(res.best, a.suff + b.pref);
+
+        return res;
+    }
+
+    void build(int node, int l, int r) {
+        if (l == r) {
+            tree[node].len = 1;
+            tree[node].pref = 1;
+            tree[node].suff = 1;
+            tree[node].best = 1;
+            tree[node].leftChar = s[l];
+            tree[node].rightChar = s[l];
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        build(node * 2, l, mid);
+        build(node * 2 + 1, mid + 1, r);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+    void update(int node, int l, int r, int pos, char c) {
+        if (l == r) {
+            tree[node].leftChar = c;
+            tree[node].rightChar = c;
+            return;
+        }
+
+        int mid = (l + r) / 2;
+
+        if (pos <= mid)
+            update(node * 2, l, mid, pos, c);
+        else
+            update(node * 2 + 1, mid + 1, r, pos, c);
+
+        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
+    }
+
+public:
+    vector<int> longestRepeating(
+        string s,
+        string queryCharacters,
+        vector<int>& queryIndices
+    ) {
+        this->s = s;
+
+        int n = s.size();
+
+        tree.resize(4 * n);
+
+        build(1, 0, n - 1);
+
+        vector<int> ans;
+
+        for (int i = 0; i < queryIndices.size(); i++) {
+            int pos = queryIndices[i];
+            char c = queryCharacters[i];
+
+            s[pos] = c;
+
+            update(1, 0, n - 1, pos, c);
+
+            ans.push_back(tree[1].best);
+        }
+
+        return ans;
+    }
+};
